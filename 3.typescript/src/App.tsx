282a09df9fc8a9d9f6a05428f25f9994da3deb69
Joy <jungheeyoo.joy@gmail.com>
2024-10-22 19:42:18 +0900

    3-3, 3-4 수정

--- 3.typescript/src/App.tsx
+++ 3.typescript/src/App.tsx
@@ -1,13 +1,44 @@
-// 3-2
-// ✅ Typing the Props
+// 3-5. Forms
 
-import Circle from './Circle';
+// event들에 타입을 추가하는 방법
+
+// FormEvent 이런건 공식 문서 또는 구글링으로 필요할 때 찾아서 씀
+// FormEvent React.FormEvent<T = Element>
+// FormEvent 내에서 하나의 Element를 보낼 수 있다
+// 이 상황에서 우리는 어떤 종류의 Element가 이 onChange 이벤트를 발생시킬지를 특정할 수 있다
+// 타입스크립트는 이 onChange 함수가 InputElement에 의해서 실행될 것을 아는 것
+
+// 셋팅
+// input의 value를 state와 연결. useState 사용.
+// onChange 함수는 input의 onChange event에 함수 onChange 연결
+// 이 onChange 함수에서 원하는 것은, evnet 에 접근하는 것
+
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('hello', value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
--- 3.typescript/src/index.tsx
+++ 3.typescript/src/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
 
 // 3-2
 // ✅ Typing the Props
+
+// TypeScirpt에게 React component를 설명하는 법
+// 어떻게 component가 필요로 하는 prop을 TypeScirpt에게 설명할 수 있는지?
+// component를 type 한다는 건, component에게 type을 준다는 뜻
+// component를 type 준다는 것은, TypeScirpt에게 뭐가 뭔지 설명해줄 수 있다는 것
+
+// const root = ReactDOM.createRoot(document.getElementById('root')); // 타입스크립트 에러 나서 수정
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+////////////////////////////////////////////////
+// 3-3
+// ✅ Optional Props
+// ✅ default props
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppCircle from './App-circle';
 
 // const root = ReactDOM.createRoot(document.getElementById('root')); // 타입스크립트 에러 나서 수정
 const root = ReactDOM.createRoot(
@@ -11,6 +35,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppCircle />
   </React.StrictMode>
 );
