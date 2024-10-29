c182e6c8480a94ac4f49685531d07b302229ca09
Joy <jungheeyoo.joy@gmail.com>
2024-10-29 19:35:29 +0900

    6-4. Recap

--- 6.state-management-1/src/atoms.ts
+++ 6.state-management-1/src/atoms.ts
@@ -17,6 +17,13 @@ export const isDarkAtom = atom({
 // ✅ 6-3.  Introduction to Recoil part Two
 // ✅ atom 의 value 수정하는 방법
 
+// ✨ useRecoilValue() : atom 의 값을 얻으려 할 때
+// 이 함수의 매개변수로 atom 을 넣음. atom 을 넣으면, atom 의 값을 줌
+
+// ✨ useSetRecoilState() :
+// 매개변수로 atom 을 받고, atom 값을 변경하는 함수를 반환함.
+// React 의 setState 와 같은 방식으로 작동 함
+
 // local state 는 모든 곳에 쓰이지는 않아야 한다
 // 단 하나의 component 가 있고, prop 을 바로 아래에 전달해준다면
 // 이것을 위해서 local state 가 필요하진 않다
