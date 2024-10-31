a5153132d07b9f61144dbfeedadcdbcc2edd39f0
Joy <jungheeyoo.joy@gmail.com>
2024-10-31 19:38:10 +0900

    8-4. Variants part Two

--- 8.animations/src/App.tsx
+++ 8.animations/src/App.tsx
@@ -84,19 +84,66 @@
 
 // export default App;
 
+// //////////////////////////////////////////////
+// // ✅ 8-3. Variants part One
+
+// import styled from 'styled-components';
+// import { motion } from 'framer-motion';
+
+// // 🔶 Variants
+// // 코드 정리: 여러 상태의 애니메이션을 한 객체로 관리해 코드가 깔끔해짐
+// // 많은 애니메이션들을 하나로 연결시켜줌
+// // **다양한 상태(예: 초기 상태, 나타나는 상태, 사라지는 상태 등)**를 정의한 다음,
+// // 해당 컴포넌트가 이 상태를 따라 애니메이션되도록 하는 것이다.
+// // 이를 통해 애니메이션 스테이지를 하나로 연결하며, 각각의 상태가 연결되도록 만들어 코드를 보다 직관적이고 유지 보수하기 쉽게 해준다.
+
+// const Wrapper = styled.div`
+//   height: 100vh;
+//   width: 100vw;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// const Box = styled(motion.div)`
+//   width: 200px;
+//   height: 200px;
+//   background-color: white;
+//   border-radius: 15px;
+//   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+// `;
+
+// // ✨ 이름은 중요하지 않음. 단지 자바스크립트의 오브젝트이다
+// // 설정을 분리된 오브젝트로 옮김
+// // 두 개의 stage
+// const myVars = {
+//   start: { scale: 0 },
+//   end: { scale: 1, rotateZ: 360, transition: { type: 'spring', delay: 0.5 } },
+// };
+
+// function App() {
+//   return (
+//     <Wrapper>
+//       <Box variants={myVars} initial="start" animate="end" />
+//     </Wrapper>
+//   );
+// }
+
+// export default App;
+
 //////////////////////////////////////////////
-// ✅ 8-3. Variants part One
-//
+// ✅ 8-4. Variants part Two
 
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-// 🔶 Variants
-// 코드 정리: 여러 상태의 애니메이션을 한 객체로 관리해 코드가 깔끔해짐
-// 많은 애니메이션들을 하나로 연결시켜줌
-// **다양한 상태(예: 초기 상태, 나타나는 상태, 사라지는 상태 등)**를 정의한 다음,
-// 해당 컴포넌트가 이 상태를 따라 애니메이션되도록 하는 것이다.
-// 이를 통해 애니메이션 스테이지를 하나로 연결하며, 각각의 상태가 연결되도록 만들어 코드를 보다 직관적이고 유지 보수하기 쉽게 해준다.
+// 🔶 부모가 자식에게 상속
+// 컴포넌트가 자식들을 갖고 있을 때, 기본 값으로 어떤 설정도 없을 때
+// 부모 컴포넌트가 variants랑 initial의 variant 이름, animate의 variant를 갖고 있을 때
+// 기본 동작으로, Motion은 initial="start" animate="end" 이것을 복사해서 자식들에게만 자동으로 붙여 줌
+// 이것은 부모가 언제 애니메이션을 끝낼지 알 수 있게 해주고
+// 자식의 애니메이션도 마칠 수 있게 해준다
+// 자식 애니메이션까지도 컨트롤할 수 있게 해준다
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -109,23 +156,64 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-// ✨ 이름은 중요하지 않음. 단지 자바스크립트의 오브젝트이다
-// 설정을 분리된 오브젝트로 옮김
-// 두 개의 stage
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: 'spring', delay: 0.5 } },
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5, // 🌱 자식들에게 딜레이 주는 것
+      staggerChildren: 0.15, // 🌱 자동적으로 자식들에게 0.5초.. 1초..delay 줌
+    },
+  },
 };
 
+// ✨ 부모와 동일한 이름을 따름.
+// 왜냐하면 기본적으로 Motion 은 자식들 각각에 initial="start" animate="end" 복사 붙여넣기 해줌
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10, // 🌱 이건 Motion에서만 쓸 수 있음
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+// ✨ 부모의 initial 값과, animate 값을 상속 받는다
+// ✨ 그래서 start, end 이름 같게 함
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
