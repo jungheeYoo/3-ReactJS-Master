c87c1eee1d0547110ee1088a112b9b1a69808e27
Joy <jungheeyoo.joy@gmail.com>
2024-11-05 23:29:07 +0900

    9-1. Header part One, 라우터, 헤더 만들기

---
+++ 9.nomflix-clone/src/App.tsx
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////
+// ✅ 9-1. Header part One
+// 라우터 만들기
+// 헤더 만들기
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Routes/Home';
+import Tv from './Routes/Tv';
+import Search from './Routes/Search';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
+// router 순서 중요
+// <Route path="/"> (/) 만 있는 경로는 처음에 넣으면 안되고 제일 마지막에 넣어야 함
+// router 가 / 이 부분과 일치하는 부분을 찾아내는데 항상 이 부분에서 true를 반환한다
+// 모든 경로가 (/) 이렇게 시작하니까 이 부분이 위에 있으면 어느곳을 가도 home으로 감
---
+++ 9.nomflix-clone/src/theme.ts
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////
+// ✅ 9-1. Header part One
+
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  red: '#E51013',
+  black: {
+    veryDark: '#141414',
+    darker: '#181818',
+    lighter: '#2F2F2F',
+  },
+  white: {
+    lighter: '#fff',
+    darker: '#e5e5e5',
+  },
+};
