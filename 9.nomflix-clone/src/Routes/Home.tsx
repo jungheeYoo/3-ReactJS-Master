582313386a92613d2913bb6d108c131894e097fc
Joy <jungheeyoo.joy@gmail.com>
2024-11-06 00:14:29 +0900

    9-2. Header part Two, 네이게이션 이동 애니매이션 효과 주기

--- 9.nomflix-clone/src/Routes/Home.tsx
+++ 9.nomflix-clone/src/Routes/Home.tsx
@@ -1,9 +1,19 @@
+// //////////////////////////////////////////////////
+// // ✅ 9-1. Header part One
+// // 라우터 만들기
+// // 헤더 만들기
+
+// function Home() {
+//   return <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}></div>;
+// }
+// export default Home;
+
 //////////////////////////////////////////////////
-// ✅ 9-1. Header part One
-// 라우터 만들기
-// 헤더 만들기
+// ✅ 9-2. Header part Two
 
 function Home() {
-  return <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}></div>;
+  return (
+    <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>home</div>
+  );
 }
 export default Home;
