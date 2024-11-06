3d901dea8f7b2c32a4aaa94a67bcff8da9f9c52b
Joy <jungheeyoo.joy@gmail.com>
2024-11-06 23:41:04 +0900

    9-5. Home Screen part One, 홈 화면 만들기 - api 가져오기

--- 9.nomflix-clone/src/Routes/Home.tsx
+++ 9.nomflix-clone/src/Routes/Home.tsx
@@ -8,12 +8,38 @@
 // }
 // export default Home;
 
-//////////////////////////////////////////////////
-// ✅ 9-2. Header part Two
+// //////////////////////////////////////////////////
+// // ✅ 9-2. Header part Two
 
+// function Home() {
+//   return (
+//     <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>home</div>
+//   );
+// }
+// export default Home;
+
+//////////////////////////////////////////////////
+// ✅ 9-5. Home Screen part One
+// 홈 화면 만들기 - api 가져오기
+
+import { useQuery } from 'react-query';
+import { getMovies } from '../api';
+
+// ✨ useQuery Hook 은 fetcher 를 사용해서 data 랑 아직 로딩중인지에 대한 알림을 전해준다
 function Home() {
+  const { data, isLoading } = useQuery(['movies', 'nowPlaying'], getMovies);
+  console.log(data, isLoading);
+
   return (
-    <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>home</div>
+    <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>Home</div>
   );
 }
+
 export default Home;
+
+// useQuery(); 에 기본적으로 키를 제공해줘야 함
+// 문자열이 될 수도 있고, 배열이 될 수도 있음
+// ['movies', 'nowPlaying'] 이 안에 있는 것은 식별자에 불과
+
+// 🔶 TheMovieDB Image가져오기
+// 이미지 파일명 앞에 https://image.tmdb.org/t/p/original/ 붙이기
---
+++ 9.nomflix-clone/src/api.ts
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////
+// ✅ 9-5. Home Screen part One
+// 홈 화면 만들기 - api 가져오기
+
+const API_KEY = '0a06d67ddbfb0da39994d1abc5bc457a';
+const BASE_PATH = 'https://api.themoviedb.org/3';
+
+// 🔶 fetcher 함수 만들기
+// fetcher 는 데이터를 받아오고 JSON을 리턴하는 함수에 불과하다
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+// api.ts 파일 만들어서
+// API_KEY 만들기
+
+// https://api.themoviedb.org/3/movie/now_playing?api_key=0a06d67ddbfb0da39994d1abc5bc457a&language=en-US&page=1&region=kr
+// URL로 요청 보내기
+// URL을 가져올 도구 만들기. 영화 목록 가져오는 도구
+
+// TheMovieDB API Key
+// https://www.themoviedb.org/settings/api?language=ko
+
+// TheMovieDB API Document
+// https://developers.themoviedb.org/3/movies/get-now-playing
