d3f221971046f3094f0e1e69db18f4fe3cbf8444
Joy <jungheeyoo.joy@gmail.com>
2024-11-07 00:25:59 +0900

    9-6. Home Screen part Two, 홈 배경 이미지 완성하기

--- 9.nomflix-clone/src/Routes/Home.tsx
+++ 9.nomflix-clone/src/Routes/Home.tsx
@@ -18,28 +18,105 @@
 // }
 // export default Home;
 
+// //////////////////////////////////////////////////
+// // ✅ 9-5. Home Screen part One
+// // 홈 화면 만들기 - api 가져오기
+
+// import { useQuery } from 'react-query';
+// import { getMovies } from '../api';
+
+// // ✨ useQuery Hook 은 fetcher 를 사용해서 data 랑 아직 로딩중인지에 대한 알림을 전해준다
+// function Home() {
+//   const { data, isLoading } = useQuery(['movies', 'nowPlaying'], getMovies);
+//   console.log(data, isLoading);
+
+//   return (
+//     <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>Home</div>
+//   );
+// }
+
+// export default Home;
+
+// // useQuery(); 에 기본적으로 키를 제공해줘야 함
+// // 문자열이 될 수도 있고, 배열이 될 수도 있음
+// // ['movies', 'nowPlaying'] 이 안에 있는 것은 식별자에 불과
+
+// // 🔶 TheMovieDB Image가져오기
+// // 이미지 파일명 앞에 https://image.tmdb.org/t/p/original/ 붙이기
+
 //////////////////////////////////////////////////
-// ✅ 9-5. Home Screen part One
-// 홈 화면 만들기 - api 가져오기
+// ✅ 9-6. Home Screen part Two
+// 홈 배경 이미지 완성하기
 
 import { useQuery } from 'react-query';
-import { getMovies } from '../api';
+import { getMovies, IGetMoviesResult } from '../api';
+import styled from 'styled-components';
+import { makeImagePath } from '../utils';
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto}); // 두 개의 배경 rgba, 이미지
+  background-size: cover;
+`;
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
 
 // ✨ useQuery Hook 은 fetcher 를 사용해서 data 랑 아직 로딩중인지에 대한 알림을 전해준다
 function Home() {
-  const { data, isLoading } = useQuery(['movies', 'nowPlaying'], getMovies);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ['movies', 'nowPlaying'],
+    getMovies
+  );
   console.log(data, isLoading);
 
+  // Cannot read properties of undefined (reading '0')
+  // TypeError: Cannot read properties of undefined (reading '0')
+  // const hasMovies = data && data.results && data.results.length > 0;
+
   return (
-    <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>Home</div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        // hasMovies && (
+        <>
+          {/* 배경 이미지를 Banner 컴포넌트(bgPhoto)에 넘김 */}
+          {/* bgPhoto 는 만들어진 utils에서 이미 경로여야함 */}
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+        // )
+      )}
+    </Wrapper>
   );
 }
 
 export default Home;
 
-// useQuery(); 에 기본적으로 키를 제공해줘야 함
-// 문자열이 될 수도 있고, 배열이 될 수도 있음
-// ['movies', 'nowPlaying'] 이 안에 있는 것은 식별자에 불과
-
-// 🔶 TheMovieDB Image가져오기
-// 이미지 파일명 앞에 https://image.tmdb.org/t/p/original/ 붙이기
+// 타입스크립트에게 useQuery 의 결과가 IGetMovieResult 타입이라고 알려줌
+// 그럼 타입스크립트는 data 안에 어떤 것들이 필요한지를 알고 있게 됨
--- 9.nomflix-clone/src/api.ts
+++ 9.nomflix-clone/src/api.ts
@@ -1,27 +1,64 @@
+// //////////////////////////////////////////////////
+// // ✅ 9-5. Home Screen part One
+// // 홈 화면 만들기 - api 가져오기
+
+// const API_KEY = '0a06d67ddbfb0da39994d1abc5bc457a';
+// const BASE_PATH = 'https://api.themoviedb.org/3';
+
+// // 🔶 fetcher 함수 만들기
+// // fetcher 는 데이터를 받아오고 JSON을 리턴하는 함수에 불과하다
+// export function getMovies() {
+//   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+//     (response) => response.json()
+//   );
+// }
+
+// // api.ts 파일 만들어서
+// // API_KEY 만들기
+
+// // https://api.themoviedb.org/3/movie/now_playing?api_key=0a06d67ddbfb0da39994d1abc5bc457a&language=en-US&page=1&region=kr
+// // URL로 요청 보내기
+// // URL을 가져올 도구 만들기. 영화 목록 가져오는 도구
+
+// // TheMovieDB API Key
+// // https://www.themoviedb.org/settings/api?language=ko
+
+// // TheMovieDB API Document
+// // https://developers.themoviedb.org/3/movies/get-now-playing
+
 //////////////////////////////////////////////////
-// ✅ 9-5. Home Screen part One
-// 홈 화면 만들기 - api 가져오기
+// ✅ 9-6. Home Screen part Two
+// 홈 배경 이미지 완성하기
+
+// ✨ 타입스크립트를 사용하기 위해 API 응답의 타입을 지정해줘야 함
+// 그러면 내가 원하는 자동완성을 사용할 수 있다
+
+// ✨ result는 내가 movie라고 부를 타입의 배열이 될 건데, 이것을 IMovie 인터페이스로 만듦
+interface IMovie {
+  id: number; // 아이디 넘버를 클릭했을 때 어디로 가고 싶은지 알고 있어야해서 ID 필요
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+// ✨ getMovies 의 결과를 나타냄
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
 
 const API_KEY = '0a06d67ddbfb0da39994d1abc5bc457a';
 const BASE_PATH = 'https://api.themoviedb.org/3';
 
-// 🔶 fetcher 함수 만들기
-// fetcher 는 데이터를 받아오고 JSON을 리턴하는 함수에 불과하다
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
   );
 }
-
-// api.ts 파일 만들어서
-// API_KEY 만들기
-
-// https://api.themoviedb.org/3/movie/now_playing?api_key=0a06d67ddbfb0da39994d1abc5bc457a&language=en-US&page=1&region=kr
-// URL로 요청 보내기
-// URL을 가져올 도구 만들기. 영화 목록 가져오는 도구
-
-// TheMovieDB API Key
-// https://www.themoviedb.org/settings/api?language=ko
-
-// TheMovieDB API Document
-// https://developers.themoviedb.org/3/movies/get-now-playing
